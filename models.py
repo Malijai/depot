0f1f4e59b7047115b06059818220600c4ed5ae94
malijai <malijai.caulet@videotron.ca>
2018-02-02 11:38:56 -0500

    Passage a Python 3 et Django2: Mise en conformite

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Dossier(models.Model):
     nomdossier = models.CharField(max_length=100)
     comment = models.CharField(max_length=200, blank=True, null=True)
-    parentId = models.ForeignKey("self")
+    parentId = models.ForeignKey("self", on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return '%s' % self.nomdossier
@@ -16,7 +16,7 @@ class Dossier(models.Model):
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d/')
     comment = models.CharField(max_length=200, blank=True, null=True)
-    dossier = models.ForeignKey(Dossier)
+    dossier = models.ForeignKey(Dossier, on_delete=models.DO_NOTHING)
     nicename = models.CharField(max_length=100, blank=True, null=True)
-    author = models.ForeignKey(User, blank=True, null=True)
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.DO_NOTHING)
     posted = models.DateTimeField(auto_now_add=True)
